ce5be8c4f469596472726c5b4620d9c315e19488
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-06-20 00:28:49 -0500

    Implementado el codigo del chat, pero presenta algunos errores, verificar funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 //modulos requeridos
-import app from "./server.js";
-import connection from "./database.js";
-
-app.listen(app.get("port"), () => {
-  console.log(`Servidor levantado en http://localhost:${app.get("port")}`);
-});
+import app from './server.js';
+import connection from './database.js';
+//configuracion necesaria para el chat
+import http from 'http';
+import { Server } from 'socket.io';
 
 connection();
+
+//Servidor del chat
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+  },
+});
+
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+  socket.on('enviar-mensaje-fron-back', (payload) => {
+    socket.broadcast.emit('enviar-mensaje-fron-back', payload);
+  });
+});
+
+//Servidor
+app.listen(app.get('port'), () => {
+  console.log(`Servidor levantado en http://localhost:${app.get('port')}`);
+});
